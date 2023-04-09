31599f52d43258f4565450b0c9ffb82d6b58cdd8
FikretAKBAS <fikret.akbas@ogr.iu.edu.tr>
2023-04-10 00:02:49 +0300

    routing request

---
+++ 7-routingrequest/app.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((request, response) => {
+  const url = request.url;
+  const method = request.method;
+
+  response.setHeader("Content-Type", "text/html");
+  if (url === "/") {
+    response.write(
+      `
+        <html>
+
+
+        <head>
+        <title>Enter Message</title>
+        
+        </head>
+        <body>
+        <form method="POST" action="/log">
+        <input type="text" name="message"/>
+        <button type="submit">Save</button>
+        
+        </form>
+        
+        </body>
+        
+        
+        </html>
+          
+        `
+    );
+
+    response.end();
+  }
+
+  if (url === "/log" && method === "POST") {
+    /**
+     * !        fs.appendFileSync("message.txt", "deneme"); üstüne ekler
+     * !        fs.writeFileSync("message.txt", "deneme");  siler ve yeniden yazar
+     */
+    // fs.writeFileSync("message.txt", "\ndeneme");
+    fs.appendFileSync("message.txt", "\ndeneme");
+    response.statusCode = 302;
+    response.setHeader("Location", "/");
+    return response.end();
+  }
+});
+
+const portNumber = 3000;
+
+server.listen(portNumber);
+
+console.log(`Listenin on port ${portNumber}...`);
