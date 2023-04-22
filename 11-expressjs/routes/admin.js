3bd273bea13308e49c8b4ed6cd6dc2e6f0f42e9d
FikretAKBAS <fikret.akbas@ogr.iu.edu.tr>
2023-04-22 23:24:16 +0300

    template engine pug is done. Next path is MVC pattern

--- 11-expressjs/routes/admin.js
+++ 11-expressjs/routes/admin.js
@@ -25,7 +25,10 @@ const products = [
 
 // /admin/add-product=> GET
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { title: "Add a New Product" });
+  res.render("add-product", {
+    title: "Add a New Product",
+    path: "/admin/add-product",
+  });
 });
 
 // /admin/add-product=> POST
--- 11-expressjs/routes/user.js
+++ 11-expressjs/routes/user.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const admin = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Homepage", products: admin.products });
+  res.render("index", {
+    title: "Homepage",
+    products: admin.products,
+    path: "/",
+  });
 });
 
 module.exports = router;
