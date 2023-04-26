c96e25e466d9324f55e45e4e535cd8511bae8a98
FikretAKBAS <fikret.akbas@ogr.iu.edu.tr>
2023-04-26 22:29:32 +0300

    models was done

--- 11-expressjs/controllers/products.js
+++ 11-expressjs/controllers/products.js
@@ -1,26 +1,7 @@
-const products = [
-  {
-    name: "Samsung S8",
-    price: 3000,
-    image: "1.jpg",
-    description: "iyi telefon",
-  },
-  {
-    name: "Samsung S7",
-    price: 2000,
-    image: "2.jpg",
-    description: "idare eder",
-  },
-  { name: "Samsung S9", price: 4000, image: "3.jpg", description: "çok iyi" },
-  {
-    name: "IPhone 7S",
-    price: 4500,
-    image: "4.jpg",
-    description: "güzel telefon",
-  },
-];
+const Product = require("../models/products");
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.getAll();
   res.render("index", {
     title: "Homepage",
     products: products,
@@ -36,12 +17,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({
-    name: req.body.name,
-    price: req.body.price,
-    image: req.body.image,
-    description: req.body.description,
-  });
-  console.log(req.body);
+  const product = new Product(
+    req.body.name,
+    req.body.price,
+    req.body.imageUrl,
+    req.body.description
+  );
+  product.saveProduct();
   res.redirect("/");
 };
--- 11-expressjs/routes/admin.js
+++ 11-expressjs/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const productsController = require("../controllers/products");
 // /admin/add-product=> GET
 router.get("/add-product", productsController.getAddProduct);
